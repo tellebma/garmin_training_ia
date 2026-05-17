06e73c7e23acddd7721bb833824e2f739548e45f
Maxime BELLET <66943979+tellebma@users.noreply.github.com>
2026-05-17 14:40:04 +0200

    build: enforce Conventional Commits via commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,24 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0],
+    'header-max-length': [2, 'always', 100],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+  },
+}
